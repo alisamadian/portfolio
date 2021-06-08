4a6d9e960f57b92ae5d5f40ff1b9398469b03e89
alisamadian <alisamadian40@gmail.com>
2021-06-08 21:52:10 +0430

    third

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -4,38 +4,93 @@ import "photoswipe/dist/photoswipe.css";
 import "photoswipe/dist/default-skin/default-skin.css";
 import { Gallery, Item } from "react-photoswipe-gallery";
 
+import imgjsbig from "./ali/certificate-js-big.jpg";
+import imgjs from "./ali/certificate-js.jpg";
+import imgresponsivebig from "./ali/certificate-responsive-big.jpg";
+import imgresponsive from "./ali/certificate-responsive.jpg";
+
 export default function Certificates() {
   return (
-    <div>
+    <div className="certificate-container">
       <Gallery>
-        <Item
-          original="https://placekitten.com/1024/768?image=1"
-          thumbnail="https://placekitten.com/80/60?image=1"
-          width="1024"
-          height="768"
-        >
-          {({ ref, open }) => (
-            <img
-              ref={ref}
-              onClick={open}
-              src="https://placekitten.com/80/60?image=1"
-            />
-          )}
-        </Item>
-        <Item
-          original="https://placekitten.com/1024/768?image=2"
-          thumbnail="https://placekitten.com/80/60?image=2"
-          width="1024"
-          height="768"
-        >
-          {({ ref, open }) => (
-            <img
-              ref={ref}
-              onClick={open}
-              src="https://placekitten.com/80/60?image=2"
-            />
-          )}
-        </Item>
+        <div className="certificate-rows">
+          <div className="certificate-row1">
+            <div className="certificate-item1">
+              <Item
+                original={imgjsbig}
+                thumbnail={imgjs}
+                width="1024"
+                height="768"
+              >
+                {({ ref, open }) => (
+                  <img ref={ref} onClick={open} src={imgjs} />
+                )}
+              </Item>
+            </div>
+            <div className="certificate-item2">
+              <Item
+                original={imgjsbig}
+                thumbnail={imgjs}
+                width="1024"
+                height="768"
+              >
+                {({ ref, open }) => (
+                  <img ref={ref} onClick={open} src={imgjs} />
+                )}
+              </Item>
+            </div>
+            <div className="certificate-item3">
+              <Item
+                original={imgjsbig}
+                thumbnail={imgjs}
+                width="1024"
+                height="768"
+              >
+                {({ ref, open }) => (
+                  <img ref={ref} onClick={open} src={imgjs} />
+                )}
+              </Item>
+            </div>
+          </div>
+          <div className="certificate-row2">
+            <div className="certificate-item4">
+              <Item
+                original={imgjsbig}
+                thumbnail={imgjs}
+                width="1024"
+                height="768"
+              >
+                {({ ref, open }) => (
+                  <img ref={ref} onClick={open} src={imgjs} />
+                )}
+              </Item>
+            </div>
+            <div className="certificate-item5">
+              <Item
+                original={imgjsbig}
+                thumbnail={imgjs}
+                width="1024"
+                height="768"
+              >
+                {({ ref, open }) => (
+                  <img ref={ref} onClick={open} src={imgjs} />
+                )}
+              </Item>
+            </div>
+            <div className="certificate-item6">
+              <Item
+                original={imgresponsivebig}
+                thumbnail={imgresponsive}
+                width="1024"
+                height="768"
+              >
+                {({ ref, open }) => (
+                  <img ref={ref} onClick={open} src={imgresponsive} />
+                )}
+              </Item>
+            </div>
+          </div>
+        </div>
       </Gallery>
     </div>
   );
